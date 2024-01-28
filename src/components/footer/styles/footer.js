9e0ab0d3c104e72439f3397bb10eaf4163cb60d6
ToQuyenPhan <duonglh024@gmail.com>
2024-01-28 22:34:27 +0700

    add new

---
+++ src/components/footer/styles/footer.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div``
+
+export const Wrapper = styled.div``
+
+export const Row = styled.div``
+
+export const Column = styled.div``
+
+export const Link = styled.div``
+
+export const Title = styled.div``
